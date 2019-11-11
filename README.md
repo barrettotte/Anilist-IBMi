5300d1039888db3a9b2e9b7828ccee6eaac03198
barrettotte <barrettotte@gmail.com>
2019-11-11 14:36:17 -0500

    Testing http in db2

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Anilist-IBMi
 
+
 A primitive RPGLE wrapper and display file built over the Anilist GraphQL API. Just for fun/practice and by no means amazing.
+
+
+This is also my first time using git on IBM i and building programs using Make.
+
+
+## References
+* IBM i + git presentation by Liam Allan https://github.com/worksofliam
+* Yum on IBM i - https://bitbucket.org/ibmi/opensource/src/master/docs/yum/
+
---
+++ workspace.sql
@@ -0,0 +1,32 @@
+﻿-- Testing out HTTP with DB2 --
+
+/*
+    * https://www.ibm.com/support/knowledgecenter/en/ssw_ibm_i_74/rzajq/rzajqhttpoverview.htm
+    * http://archive.ibmsystemsmag.com/blogs/i-can/january-2017/json_table-function/
+    * Systools.HttpPostClob(:url, :header, :body)
+*/
+
+
+-- GET requests testing -- 
+values Systools.HttpGetClob('https://api.ratesapi.io/api/latest?base=USD', '');
+select * from table(Systools.HttpGetClobVerbose('https://api.ratesapi.io/api/latest?base=USD', ''));
+
+
+-- Testing Anilist query --
+select * from table(Systools.HttpPostClobVerbose('https://graphql.anilist.co',
+    cast('<httpHeader><header name="Content-Type" value="application/json"/></httpHeader>' as clob),
+    cast('{"query": "{User(search:\"barrettotte\"){id name siteUrl stats{watchedTime}}}"}' as clob))
+);
+-- this will be built dynamically in SQLRPGLE --
+
+
+/* 
+    Scalar functions to make:
+    - simple graphQL query builder?
+    - search for user
+    - get user by id
+    - get anime by id
+    - search for anime
+    - get list by user id  
+*/
+
